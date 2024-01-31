8030a5da45a8cffdf15a837cc79b8356745fe33c
ToadyGamer <castellierflorian@gmail.com>
2024-01-31 17:24:49 +0100

    ADD - Get who don't get the data

--- Web app/app.js
+++ Web app/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-const myMiddleware = ('middlewares/myMiddleware');
-
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connexion à la base de données réussie');
@@ -12,9 +10,31 @@ mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useU
     console.error('Erreur de connexion à la base de données :', err);
   });
 
-app.get('/exemple', myMiddleware, (req, res) => {
+const avignonSchema = new mongoose.Schema({
+  nom: String,
+  localisation: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  }
+});
+avignonSchema.index({ localisation: '2dsphere' });
 
-  res.send('Ceci est un exemple de route GET avec MongoDB');
+const Avignon = mongoose.model('avignon', avignonSchema);
+
+app.get('/data', async (req, res) => {
+  try {
+    const data = await Avignon.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 const port = 3000;
