76d6e930d1eb903d03463b7a6bad3935ed371344
ToadyGamer <castellierflorian@gmail.com>
2024-01-31 16:47:03 +0100

    ADD - Middleware

--- Web app/app.js
+++ Web app/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const myMiddleware = ('middlewares/myMiddleware');
+
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connexion à la base de données réussie');
@@ -10,11 +12,9 @@ mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useU
     console.error('Erreur de connexion à la base de données :', err);
   });
 
-app.get('/exemple', (req, res) => {
+app.get('/exemple', myMiddleware, (req, res) => {
 
   res.send('Ceci est un exemple de route GET avec MongoDB');
-
-        
 });
 
 const port = 3000;
